35ea9001c6e7333bbd8515569b2710c34f7d072e
Michael Gwynne <mg@paddle.com>
2015-09-28 16:14:14 +0100

    Removed some unneeded modules

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
   * Clone repo
   * Change into directory
   * `vagrant up`
-  * Visit http://localhost:9080/wp-admin
+  * Visit http://localhost:9080/wp-admin/
   * Log in using "admin" and "Passw0rd"
--- scripts/vagrant/aptitude.sh
+++ scripts/vagrant/aptitude.sh
@@ -7,7 +7,6 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password_again passwo
 debconf-set-selections <<< 'debconf shared/accepted-oracle-license-v1-1 select true'
 debconf-set-selections <<< 'debconf shared/accepted-oracle-license-v1-1 seen true'
 
-apt-get install -y apache2 mysql-server mysql-client php5 php5-mcrypt php5-intl php5-odbc php5-mysql php5-gmp php5-curl redis-server realpath git xvfb \
-    ruby1.9.1-dev make zlib1g-dev libicu-dev build-essential \
+apt-get install -y apache2 mysql-server mysql-client php5 php5-mcrypt php5-intl php5-mysql php5-curl \
 
 php5enmod mcrypt
